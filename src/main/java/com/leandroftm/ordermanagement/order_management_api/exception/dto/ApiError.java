8828e912108d5f7af50ee083fbcd19834e90cba5
Leandro Torres <leandroftm@gmail.com>
2026-04-22 03:58:17 -0300

    feat: created dto for api error

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/dto/ApiError.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/dto/ApiError.java
@@ -1,4 +1,16 @@
 package com.leandroftm.ordermanagement.order_management_api.exception.dto;
 
-public record ApiError() {
+import com.leandroftm.ordermanagement.order_management_api.exception.enums.ErrorCode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ApiError(
+        int status,
+        String error,
+        ErrorCode errorCode,
+        List<String> messages,
+        String path,
+        LocalDateTime timestamp
+) {
 }
