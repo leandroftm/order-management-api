1298c5402ec125cfd57ffc165efbbdd3c084dc44
Leandro Torres <leandroftm@gmail.com>
2026-04-22 04:00:21 -0300

    feat: created product exception for insuficient stock

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/domain/product/InsufficientStockException.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/domain/product/InsufficientStockException.java
@@ -1,12 +1,13 @@
 package com.leandroftm.ordermanagement.order_management_api.exception.domain.product;
 
 import com.leandroftm.ordermanagement.order_management_api.exception.domain.DomainException;
+import com.leandroftm.ordermanagement.order_management_api.exception.enums.ErrorCode;
 
-public class InsuficientStockException extends DomainException {
-    public InsuficientStockException(String message) {
+public class InsufficientStockException extends DomainException {
+    public InsufficientStockException() {
         super(
                 "Insufficient stock",
-                
+                ErrorCode.INSUFFICIENT_STOCK
         );
     }
 }
