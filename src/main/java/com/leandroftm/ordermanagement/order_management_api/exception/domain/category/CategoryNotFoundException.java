e32f33a0375f1fefa2d0f33c339cec05d1b9fd66
Leandro Torres <leandroftm@gmail.com>
2026-04-22 03:59:50 -0300

    feat: created category exceptions for category not found and category is null

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/domain/category/CategoryNotFoundException.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/domain/category/CategoryNotFoundException.java
@@ -1,7 +1,13 @@
 package com.leandroftm.ordermanagement.order_management_api.exception.domain.category;
 
-public class CategoryNotFoundException extends RuntimeException {
-  public CategoryNotFoundException(String message) {
-    super(message);
-  }
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.NotFoundException;
+import com.leandroftm.ordermanagement.order_management_api.exception.enums.ErrorCode;
+
+public class CategoryNotFoundException extends NotFoundException {
+    public CategoryNotFoundException() {
+        super(
+                "Category not found",
+                ErrorCode.CATEGORY_NOT_FOUND
+        );
+    }
 }
--- src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/domain/category/CategoryNullException.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/domain/category/CategoryNullException.java
@@ -1,7 +1,13 @@
 package com.leandroftm.ordermanagement.order_management_api.exception.domain.category;
 
-public class CategoryNullException extends RuntimeException {
-  public CategoryNullException(String message) {
-    super(message);
-  }
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.DomainException;
+import com.leandroftm.ordermanagement.order_management_api.exception.enums.ErrorCode;
+
+public class CategoryNullException extends DomainException {
+    public CategoryNullException() {
+        super(
+                "Category cannot be null",
+                ErrorCode.CATEGORY_NULL
+        );
+    }
 }
