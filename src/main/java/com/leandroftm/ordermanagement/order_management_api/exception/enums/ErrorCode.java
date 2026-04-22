ad29e9f8bd17a9d4a9a4b7608fa8a6e7f458b25a
Leandro Torres <leandroftm@gmail.com>
2026-04-22 04:00:43 -0300

    feat: created global error codes enum

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/enums/ErrorCode.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/exception/enums/ErrorCode.java
@@ -1,4 +1,14 @@
 package com.leandroftm.ordermanagement.order_management_api.exception.enums;
 
 public enum ErrorCode {
+    UNEXPECTED_ERROR,
+    VALIDATION_ERROR,
+    INVALID_CREDENTIALS,
+    CATEGORY_NOT_FOUND,
+    CATEGORY_NULL,
+    CATEGORY_ALREADY_EXISTS,
+    ORDER_NOT_FOUND,
+    PRODUCT_NOT_FOUND,
+    PRODUCT_ALREADY_EXISTS,
+    INSUFFICIENT_STOCK
 }
