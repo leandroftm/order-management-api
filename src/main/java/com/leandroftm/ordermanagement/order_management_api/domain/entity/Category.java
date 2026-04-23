34557d3e05b1fb5df3acd67c169602dd5cd2190f
Leandro Torres <leandroftm@gmail.com>
2026-04-23 00:47:33 -0300

    feat: added active parameter, constructor, entity update, entity enable and disable

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/entity/Category.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/entity/Category.java
@@ -1,16 +1,17 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.entity;
 
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.category.CategoryAlreadyActiveException;
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.category.InvalidCategoryNameException;
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.product.CategoryAlreadyInactiveException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @Table(name = "categories")
 public class Category {
@@ -19,6 +20,45 @@ public class Category {
     private Long id;
     @Column(unique = true, nullable = false)
     private String name;
+    @Column(nullable = false)
+    private boolean active;
     @OneToMany(mappedBy = "category")
     private List<Product> products = new ArrayList<>();
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.active = true;
+    }
+
+    public void update(String name) {
+        if (name == null || name.isBlank())
+            throw new InvalidCategoryNameException();
+            this.name = name;
+    }
+
+    public void disable() {
+        if (!this.active) {
+            throw new CategoryAlreadyInactiveException();
+        }
+        deactivateInternal();
+    }
+
+    public void enable() {
+        if (this.active) {
+            throw new CategoryAlreadyActiveException();
+        }
+        activateInternal();
+    }
+
+    private void activateInternal() {
+        this.active = true;
+    }
+
+    private void deactivateInternal() {
+        this.active = false;
+    }
 }
