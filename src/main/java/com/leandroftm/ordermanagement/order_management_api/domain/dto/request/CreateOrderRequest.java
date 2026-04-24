f1f69c138158190caaafeed75cafdab074eba614
Leandro Torres <leandroftm@gmail.com>
2026-04-23 23:12:31 -0300

    feat: created request/response dtos for order

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/request/CreateOrderRequest.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/request/CreateOrderRequest.java
@@ -1,4 +1,14 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.dto.request;
 
-public record CreateOrderRequest() {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record CreateOrderRequest(
+        @NotNull
+        Long userId,
+        @NotEmpty
+        List<CreateOrderItemRequest> orderItems
+) {
 }
--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/response/OrderResponse.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/response/OrderResponse.java
@@ -1,4 +1,28 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.dto.response;
 
-public record OrderResponse() {
+import com.leandroftm.ordermanagement.order_management_api.domain.entity.Order;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Status;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OrderResponse(
+        Long id,
+        BigDecimal totalAmount,
+        LocalDateTime createdAt,
+        Status status,
+        Long userId,
+        List<OrderItemResponse> orderItems
+) {
+    public OrderResponse(Order order) {
+        this(
+                order.getId(),
+                order.getTotalAmount(),
+                order.getCreatedAt(),
+                order.getStatus(),
+                order.getUser().getId(),
+                order.getOrderItems().stream().map(OrderItemResponse::new).toList()
+        );
+    }
 }
