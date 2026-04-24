9b44527c33b8e3f5bde7b6abfcf0927a733319e9
Leandro Torres <leandroftm@gmail.com>
2026-04-23 23:09:42 -0300

    feat: added min/max annotations for discount, created item total price calculator

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/entity/Order.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/entity/Order.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/entity/OrderItem.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/entity/OrderItem.java
@@ -1,11 +1,14 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Entity
 @Getter
@@ -21,8 +24,10 @@ public class OrderItem {
     private Integer quantity;
     @Column(nullable = false)
     private BigDecimal unitPrice = BigDecimal.ZERO;
+    @Min(0)
+    @Max(100)
     @Column(nullable = false)
-    private BigDecimal discount;
+    private BigDecimal discount = BigDecimal.ZERO;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
@@ -30,11 +35,29 @@ public class OrderItem {
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    //Move to service later
     @PrePersist
     public void snapshotPrice() {
         if (product != null) {
             this.unitPrice = product.getPrice();
         }
+    }
+
+    public BigDecimal calcItemTotalPrice() {
+        if (product == null || product.getPrice() == null || quantity == null) {
+            return BigDecimal.ZERO;
+        }
+
+        //(price * quantity)
+        BigDecimal grossTotal = product.getPrice().multiply(BigDecimal.valueOf(quantity));
+
+        BigDecimal currentDiscount = (discount == null) ? BigDecimal.ZERO : discount;
+
+        //calculate discount
+        BigDecimal discountMultiplier = currentDiscount.divide(new BigDecimal("100"), 4, RoundingMode.HALF_UP);
+
+        //calculate discount amount
+        BigDecimal discountAmount = grossTotal.multiply(discountMultiplier);
+
+        return grossTotal.subtract(discountAmount).setScale(2, RoundingMode.HALF_UP);
     }
 }
