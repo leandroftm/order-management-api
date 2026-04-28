5d370b07940ed80a6c66075c096585eee8565671
Leandro Torres <leandroftm@gmail.com>
2026-04-28 00:38:29 -0300

    feat: find order by user id created

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/OrderRepository.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.leandroftm.ordermanagement.order_management_api.repository;
 
 import com.leandroftm.ordermanagement.order_management_api.domain.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    Page<Order> findByUserId(Long userId, Pageable pageable);
 }
