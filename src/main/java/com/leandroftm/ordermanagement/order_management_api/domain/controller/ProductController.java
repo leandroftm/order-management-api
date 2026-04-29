98f4f84506d408433fe314e13bfc66038dcb44f8
Leandro Torres <leandroftm@gmail.com>
2026-04-29 04:02:50 -0300

    feat: created controller for product, with the endpoints GET all products, GET product by id, PUT update product, PATCH update product price, POST increase product stock, POST decrease product stock, POST enable product, POST disable product

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/ProductController.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/ProductController.java
@@ -1,4 +1,72 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.controller;
 
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.update.UpdateProductRequest;
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.ProductResponse;
+import com.leandroftm.ordermanagement.order_management_api.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getAllProducts(@PageableDefault(size = 10, sort = "name") Pageable pageable) {
+        Page<ProductResponse> page = productService.findAll(pageable);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long id) {
+        ProductResponse response = productService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    //ADMIN ROLE
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody UpdateProductRequest request) {
+        productService.update(id, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/price")
+    public ResponseEntity<Void> updatePrice(@PathVariable Long id, @RequestBody BigDecimal newPrice) {
+        productService.updatePrice(id, newPrice);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/stock/increase")
+    public ResponseEntity<Void> increaseStock(@PathVariable Long id, @RequestParam Integer quantity) {
+        productService.increaseStock(id, quantity);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/stock/decrease")
+    public ResponseEntity<Void> decreaseStock(@PathVariable Long id, @RequestParam Integer quantity) {
+        productService.decreaseStock(id, quantity);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/enable")
+    public ResponseEntity<Void> enableProduct(@PathVariable Long id) {
+        productService.enableProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/disable")
+    public ResponseEntity<Void> disableProduct(@PathVariable Long id) {
+        productService.disableProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+    //END ADMIN ROLE
 }
