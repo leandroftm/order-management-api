0193bb1afd1f86770abed6d0639e4097efe665fb
Leandro Torres <leandroftm@gmail.com>
2026-04-28 22:51:37 -0300

    feat: added find orders by user id and order status and find orders by status

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/request/update/UpdateOrderStatusRequest.java
+++
@@ -1,8 +0,0 @@
-package com.leandroftm.ordermanagement.order_management_api.domain.dto.request.update;
-
-import com.leandroftm.ordermanagement.order_management_api.domain.enums.Status;
-
-public record UpdateOrderStatusRequest(
-        Status status
-) {
-}
--- src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/OrderRepository.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/OrderRepository.java
@@ -1,11 +1,15 @@
 package com.leandroftm.ordermanagement.order_management_api.repository;
 
 import com.leandroftm.ordermanagement.order_management_api.domain.entity.Order;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface OrderRepository extends JpaRepository<Order,Long> {
     Page<Order> findByUserId(Long userId, Pageable pageable);
+
+    Page<Order> findByUserIdAndStatus(Long userId, Status status, Pageable pageable);
+
+    Page<Order> findByStatus(Status status, Pageable pageable);
 }
