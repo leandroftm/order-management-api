6ded1a094a2834c2a543011260771ffbef138670
Leandro Torres <leandroftm@gmail.com>
2026-04-29 21:53:33 -0300

    feat: created controller for order, with: POST create, GET by user id and order status, GET by user id, GET by order id, POST pay order, POST cancel order, ADMIN GET all orders

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/OrderController.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/OrderController.java
@@ -1,4 +1,72 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.controller;
 
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.create.CreateOrderRequest;
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.OrderResponse;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Status;
+import com.leandroftm.ordermanagement.order_management_api.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestParam Long userId, @RequestBody @Valid CreateOrderRequest request) {
+        Long id = orderService.createOrder(userId, request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    //will receive user from authorization later
+    @GetMapping("/user/{userId}/status")
+    public ResponseEntity<Page<OrderResponse>> getByUserAndStatus(@PathVariable Long userId, @RequestParam Status status, @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(orderService.getOrdersByUserAndOrderStatus(userId, status, pageable));
+    }
+
+    //will receive user from authorization later
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Page<OrderResponse>> getByUser(@PathVariable Long userId, @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(orderService.getOrdersByUser(userId, pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.getOrder(id));
+    }
+
+    @PostMapping("/{id}/pay")
+    public ResponseEntity<Void> pay(@PathVariable Long id) {
+        orderService.payOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/cancel")
+    public ResponseEntity<Void> cancel(@PathVariable Long id) {
+        orderService.cancelOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //ADMIN
+    @GetMapping
+    public ResponseEntity<Page<OrderResponse>> getAll(@PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(orderService.getAllOrders(pageable));
+    }
 }
