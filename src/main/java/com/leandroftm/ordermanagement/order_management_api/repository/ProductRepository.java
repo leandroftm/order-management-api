e69b8bfa170148e07bf45989e7c69b70b6874c58
Leandro Torres <leandroftm@gmail.com>
2026-05-01 00:06:41 -0300

    fix: fixed missing pageable parameter on find all by category

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/ProductRepository.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/ProductRepository.java
@@ -3,13 +3,14 @@ package com.leandroftm.ordermanagement.order_management_api.repository;
 import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.ProductResponse;
 import com.leandroftm.ordermanagement.order_management_api.domain.entity.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    Page<ProductResponse> findAllByCategory(Long categoryId);
+    Page<ProductResponse> findAllByCategory(Long categoryId, Pageable pageable);
 
     Optional<Product> findByIdAndCategoryId(Long productId, Long categoryId);
 
