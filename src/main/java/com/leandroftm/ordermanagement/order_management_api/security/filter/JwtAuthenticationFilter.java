3a887bf814ea84fa0063f940651671aa338ca72f
Leandro Torres <leandroftm@gmail.com>
2026-05-01 00:23:06 -0300

    feat: JWT authentication filter created, TODO add user credentials

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,75 @@
 package com.leandroftm.ordermanagement.order_management_api.security.filter;
 
-public class JwtAuthenticationFilter {
+import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import com.leandroftm.ordermanagement.order_management_api.repository.UserRepository;
+import com.leandroftm.ordermanagement.order_management_api.security.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        if (path.equals("/v3/api-docs") ||
+                path.contains("/swagger-ui")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = recoverToken(request);
+
+        if (token != null) {
+            String userId = tokenService.validateToken(token);
+
+            if (userId != null) {
+                User user = userRepository.findById(Long.valueOf(userId)).orElse(null);
+                if (user == null) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        List.of()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return header.substring(7);
+    }
 }
