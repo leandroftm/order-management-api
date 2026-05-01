e65c94a3fa71932236a1ad4318531f3cfc7788fd
Leandro Torres <leandroftm@gmail.com>
2026-05-01 00:21:35 -0300

    feat: Service for authentication created, with: authenticate(loginrequest)

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/AuthService.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/AuthService.java
@@ -1,4 +1,26 @@
 package com.leandroftm.ordermanagement.order_management_api.security.service;
 
+import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.user.InvalidCredentialsException;
+import com.leandroftm.ordermanagement.order_management_api.security.dto.LoginRequest;
+import com.leandroftm.ordermanagement.order_management_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final TokenService tokenService;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public String authenticate(LoginRequest request) {
+        User user = userService.findUserByEmail(request.email());
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+            throw new InvalidCredentialsException();
+        }
+        return tokenService.generateToken(user);
+    }
 }
