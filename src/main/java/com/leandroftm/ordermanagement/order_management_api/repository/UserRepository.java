cc0ad0534e1b75ab91e697364585c787e92f0312
Leandro Torres <leandroftm@gmail.com>
2026-05-01 00:05:23 -0300

    feat: repository for user created

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/UserRepository.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/UserRepository.java
@@ -1,7 +1,17 @@
 package com.leandroftm.ordermanagement.order_management_api.repository;
 
 import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+    Page<User> findByRoleIgnoreCase(Role role, Pageable pageable);
+
+    Optional<User> findByEmailIgnoreCase(String email);
 }
