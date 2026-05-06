35508b568b55b31086283bd6834b49bebc1386fa
Leandro Torres <leandroftm@gmail.com>
2026-05-05 22:47:13 -0300

    feat: created controller for authentication, with POST register, POST login

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/controller/AuthController.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/controller/AuthController.java
@@ -1,6 +1,16 @@
-package com.leandroftm.ordermanagement.order_management_api.security;
+package com.leandroftm.ordermanagement.order_management_api.security.controller;
 
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.create.CreateUserRequest;
+import com.leandroftm.ordermanagement.order_management_api.security.dto.LoginRequest;
+import com.leandroftm.ordermanagement.order_management_api.security.dto.LoginResponse;
+import com.leandroftm.ordermanagement.order_management_api.security.service.AuthService;
+import com.leandroftm.ordermanagement.order_management_api.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> registerUser(@Valid @RequestBody CreateUserRequest request) {
+        userService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        String token = authService.authenticate(request);
+        return ResponseEntity.ok(new LoginResponse(token, "Bearer"));
+    }
 }
