0854a7131f58ed5f71f2656b9b2fd07d7a2ba2f7
Leandro Torres <leandroftm@gmail.com>
2026-05-06 22:38:56 -0300

    refactor: changed to get jwt key properties, changed to use user details instead of user, changed key to receive user getname(user email) instead of user id)

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/TokenService.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/TokenService.java
@@ -3,23 +3,31 @@ package com.leandroftm.ordermanagement.order_management_api.security.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 @Service
-public class JwtService {
-    private final String SECRET = "my-secret-key";
+public class TokenService {
+    @Value("${jwt.secret}")
+    private String secret;
 
     public String generateToken(UserDetails user) {
-      return Jwts.builder
+       return JWT.create()
+               .withSubject(user.getUsername())
+               .withExpiresAt(Instant.now().plus(2, ChronoUnit.HOURS))
+               .sign(Algorithm.HMAC256(secret));
     }
 
-    public String validateToken(String token) {
+    public String extractUsername(String token) {
         try {
             return JWT.require(Algorithm.HMAC256(secret))
                     .build()
                     .verify(token)
-                    .getSubject(); //return userId
+                    .getSubject(); //return user email
         }catch (JWTVerificationException e){
             return null;
         }
