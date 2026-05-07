9b668c66cb7bde08bc87e9e8a620ca44809c3a8e
Leandro Torres <leandroftm@gmail.com>
2026-05-06 22:46:25 -0300

    feat: add optional find order by id and user id

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/OrderRepository.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/OrderRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order,Long> {
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findByUserId(Long userId, Pageable pageable);
 
     Page<Order> findByUserIdAndStatus(Long userId, Status status, Pageable pageable);
 
     Page<Order> findByStatus(Status status, Pageable pageable);
+
+    Optional<Order> findByIdAndUserId(Long orderId, Long id);
 }
