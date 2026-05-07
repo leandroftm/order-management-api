79a4e0864c10b7d3f6912741132845e7fa1b749f
Leandro Torres <leandroftm@gmail.com>
2026-05-06 22:35:27 -0300

    feat: added security service for user details

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/CustomUserDetailsService.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.leandroftm.ordermanagement.order_management_api.security.service;
 
-public class CustomUserDetailsService {
+import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import com.leandroftm.ordermanagement.order_management_api.exception.domain.user.UserNotFoundException;
+import com.leandroftm.ordermanagement.order_management_api.repository.UserRepository;
+import com.leandroftm.ordermanagement.order_management_api.security.entity.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailIgnoreCase(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        return new UserPrincipal(user);
+    }
 }
