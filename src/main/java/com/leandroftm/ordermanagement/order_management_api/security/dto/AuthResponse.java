27f9bd91391f82037605e15f660e434146a49548
Leandro Torres <leandroftm@gmail.com>
2026-05-06 22:32:05 -0300

    feat: added jwt secret key value

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/dto/AuthResponse.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/dto/AuthResponse.java
@@ -1,6 +1,6 @@
 package com.leandroftm.ordermanagement.order_management_api.security.dto;
 
-public record LoginResponse(
+public record AuthResponse(
         String token,
         String type
 ) {
--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/TokenService.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/service/TokenService.java
@@ -3,21 +3,15 @@ package com.leandroftm.ordermanagement.order_management_api.security.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-
 @Service
-public class TokenService {
-    private final String secret = "my-secret-key";
+public class JwtService {
+    private final String SECRET = "my-secret-key";
 
-    public String generateToken(User user) {
-       return JWT.create()
-               .withSubject(user.getId().toString())
-               .withExpiresAt(Instant.now().plus(2, ChronoUnit.HOURS))
-               .sign(Algorithm.HMAC256(secret));
+    public String generateToken(UserDetails user) {
+      return Jwts.builder
     }
 
     public String validateToken(String token) {
