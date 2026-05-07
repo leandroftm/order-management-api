cb90946b0730d803e3af82e335e5397acb1ec29e
Leandro Torres <leandroftm@gmail.com>
2026-05-06 23:02:26 -0300

    refactor: added pre authorize when role is ADMIN, changed mapping /{id} to /me, get user by role merged with get all users, removed assign user role

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/UserController.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/UserController.java
@@ -1,23 +1,20 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.controller;
 
-import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.create.CreateUserRequest;
-import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.update.UpdateUserRoleRequest;
 import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.OrderResponse;
 import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.UserResponse;
 import com.leandroftm.ordermanagement.order_management_api.domain.enums.Role;
 import com.leandroftm.ordermanagement.order_management_api.domain.enums.Status;
+import com.leandroftm.ordermanagement.order_management_api.security.entity.UserPrincipal;
 import com.leandroftm.ordermanagement.order_management_api.service.OrderService;
 import com.leandroftm.ordermanagement.order_management_api.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
@@ -28,60 +25,56 @@ public class UserController {
 
     private final OrderService orderService;
 
-    @PostMapping
-    public ResponseEntity<Void> create(@Valid @RequestBody CreateUserRequest request) {
-        Long id = userService.create(request);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(id)
-                .toUri();
+    //POST create user moved to auth
 
-        return ResponseEntity.created(location).build();
-    }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
-    public ResponseEntity<Page<UserResponse>> getAll(@PageableDefault(size = 10, sort = "email") Pageable pageable) {
-        return ResponseEntity.ok(userService.getUsers(pageable));
+    public ResponseEntity<Page<UserResponse>> getUsers(@RequestParam(required = false) Role role, @PageableDefault(size = 10, sort = "email") Pageable pageable) {
+        return ResponseEntity.ok(userService.getUsers(role, pageable));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUser(id));
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getMe(@AuthenticationPrincipal UserPrincipal user) {
+        return ResponseEntity.ok(userService.getUser(user.getId()));
     }
 
-    @GetMapping("/role")
-    public ResponseEntity<Page<UserResponse>> getByRole(@RequestParam Role role, @PageableDefault(size = 10, sort = "email") Pageable pageable) {
-        return ResponseEntity.ok(userService.getUsersByRole(role, pageable));
-    }
+    //get by role merged with get all users
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{id}/enable")
     public ResponseEntity<Void> enable(@PathVariable Long id) {
         userService.enable(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{id}/disable")
     public ResponseEntity<Void> disable(@PathVariable Long id) {
         userService.disable(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{id}/role")
-    public ResponseEntity<Void> assignRole(@PathVariable Long id, @RequestBody UpdateUserRoleRequest request) {
-        userService.assignRole(id, request);
-        return ResponseEntity.noContent().build();
+    //REMOVED assign user role
+//    @PostMapping("/{id}/role")
+//    public ResponseEntity<Void> assignRole(@PathVariable Long id, @RequestBody UpdateUserRoleRequest request) {
+//        userService.assignRole(id, request);
+//        return ResponseEntity.noContent().build();
+//    }
+
+    @GetMapping("/me/orders")
+    public ResponseEntity<Page<OrderResponse>> getOrdersByUser(@AuthenticationPrincipal UserPrincipal user, @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(orderService.getOrdersByUser(user.getId(), pageable));
     }
 
-    //will receive user from authorization later
-    @GetMapping("/{id}/orders")
-    public ResponseEntity<Page<OrderResponse>> getOrderByUser(@PathVariable Long id, @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
-        return ResponseEntity.ok(orderService.getOrdersByUser(id, pageable));
+    @GetMapping("/me/orders/status")
+    public ResponseEntity<Page<OrderResponse>> getOrdersByUserAndStatus(@AuthenticationPrincipal UserPrincipal user, @RequestParam Status status, @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(orderService.getOrdersByUserAndOrderStatus(user.getId(), status, pageable));
     }
 
-    //will receive user from authorization later
-    @GetMapping("/{id}/orders/status")
-    public ResponseEntity<Page<OrderResponse>> getByUserAndStatus(@PathVariable Long id, @RequestParam Status status, @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
-        return ResponseEntity.ok(orderService.getOrdersByUserAndOrderStatus(id, status, pageable));
+
+    @GetMapping("/me/orders/{orderId}")
+    public ResponseEntity<OrderResponse> getByUserId(@PathVariable Long orderId, @AuthenticationPrincipal UserPrincipal user) {
+        OrderResponse response = orderService.findByIdAndUserId(orderId, user.getId());
+        return ResponseEntity.ok(response);
     }
 }
