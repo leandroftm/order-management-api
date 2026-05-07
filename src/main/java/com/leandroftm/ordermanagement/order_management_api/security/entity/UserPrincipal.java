996ebb71434e87773b45a456a30bd6bb9ddf96e2
Leandro Torres <leandroftm@gmail.com>
2026-05-06 22:33:21 -0300

    feat: added UserPrincipal entity for security

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/entity/UserPrincipal.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/entity/UserPrincipal.java
@@ -1,4 +1,42 @@
 package com.leandroftm.ordermanagement.order_management_api.security.entity;
 
-public class UserPrincipal {
+import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
 }
