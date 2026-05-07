ecc8617810a1a9423679ef816cb0e89372817405
Leandro Torres <leandroftm@gmail.com>
2026-05-06 22:56:32 -0300

    refactor: added pre authorize when role is ADMIN, changed from BigDecimal new price to update price request on updatePrice

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/ProductController.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.controller;
 
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.update.UpdatePriceRequest;
 import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.update.UpdateProductRequest;
 import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.ProductResponse;
 import com.leandroftm.ordermanagement.order_management_api.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -18,7 +21,6 @@ import java.math.BigDecimal;
 public class ProductController {
 
     private final ProductService productService;
-
 
     @GetMapping
     public ResponseEntity<Page<ProductResponse>> getAllProducts(@PageableDefault(size = 10, sort = "name") Pageable pageable) {
@@ -32,41 +34,45 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
-    //ADMIN ROLE
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody UpdateProductRequest request) {
         productService.update(id, request);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{id}/price")
-    public ResponseEntity<Void> updatePrice(@PathVariable Long id, @RequestBody BigDecimal newPrice) {
-        productService.updatePrice(id, newPrice);
+    public ResponseEntity<Void> updatePrice(@PathVariable Long id, @RequestBody @Valid UpdatePriceRequest request) {
+        productService.updatePrice(id, request);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{id}/stock/increase")
     public ResponseEntity<Void> increaseStock(@PathVariable Long id, @RequestParam Integer quantity) {
         productService.increaseStock(id, quantity);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{id}/stock/decrease")
     public ResponseEntity<Void> decreaseStock(@PathVariable Long id, @RequestParam Integer quantity) {
         productService.decreaseStock(id, quantity);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{id}/enable")
     public ResponseEntity<Void> enableProduct(@PathVariable Long id) {
         productService.enableProduct(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{id}/disable")
     public ResponseEntity<Void> disableProduct(@PathVariable Long id) {
         productService.disableProduct(id);
         return ResponseEntity.noContent().build();
     }
-    //END ADMIN ROLE
 }
