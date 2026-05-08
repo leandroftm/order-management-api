379c322cead616218a381d89c0bf891beb614a03
Leandro Torres <leandroftm@gmail.com>
2026-05-08 01:07:10 -0300

    feat: add conditional if category exists by name and ignoring the category id

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/CategoryRepository.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/repository/CategoryRepository.java
@@ -1,8 +1,12 @@
 package com.leandroftm.ordermanagement.order_management_api.repository;
 
 import com.leandroftm.ordermanagement.order_management_api.domain.entity.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
+
     boolean existsByNameIgnoreCase(String name);
 }
