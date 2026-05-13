cf94fe8af7c3313e4877a30b099016c680aca622
Leandro Torres <leandroftm@gmail.com>
2026-05-13 03:49:19 -0300

    feat: added dto request for user update

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/request/update/UpdateUserRequest.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/domain/dto/request/update/UpdateUserRequest.java
@@ -1,10 +1,15 @@
 package com.leandroftm.ordermanagement.order_management_api.domain.dto.request.update;
 
 import com.leandroftm.ordermanagement.order_management_api.domain.enums.Role;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record UpdateUserRoleRequest(
-        @NotNull
-        Role role
+public record UpdateUserRequest(
+        @Size(max = 100)
+        String email,
+        @NotBlank
+        @Size(min = 6, max = 20)
+        String password
 ) {
 }
