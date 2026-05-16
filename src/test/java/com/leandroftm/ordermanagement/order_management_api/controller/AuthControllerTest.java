3ed2963c36308bce5bd1fd23ee7e37ee520860b6
Leandro Torres <leandroftm@gmail.com>
2026-05-16 00:19:11 -0300

    feat: created test for auth controller, with: POST login, POST register

---
+++ src/test/java/com/leandroftm/ordermanagement/order_management_api/controller/AuthControllerTest.java
@@ -0,0 +1,68 @@
+package com.leandroftm.ordermanagement.order_management_api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.create.CreateUserRequest;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Role;
+import com.leandroftm.ordermanagement.order_management_api.security.dto.LoginRequest;
+import com.leandroftm.ordermanagement.order_management_api.security.service.AuthService;
+import com.leandroftm.ordermanagement.order_management_api.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthService authService;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldLoginSuccessfully() throws Exception {
+        LoginRequest request = new LoginRequest("test@test.com", "123456");
+
+        when(authService.authenticate(any())).thenReturn("test-token");
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("test-token"));
+    }
+
+    @Test
+    void shouldRegisterUserSuccessfully() throws Exception {
+        CreateUserRequest request = new CreateUserRequest(
+                "test@test.com",
+                "123456",
+                Role.ADMIN
+        );
+
+        when(userService.create(request)).thenReturn(any(Long.class));
+
+       mockMvc.perform(post("/auth/register")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(objectMapper.writeValueAsString(request)))
+               .andExpect(status().isCreated());
+    }
+}
