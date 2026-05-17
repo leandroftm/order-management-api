125443d096ab643e6d9ff1734c86b826be108d51
Leandro Torres <leandroftm@gmail.com>
2026-05-17 02:37:12 -0300

    feat: add enable method security

--- src/main/java/com/leandroftm/ordermanagement/order_management_api/security/config/SecurityConfig.java
+++ src/main/java/com/leandroftm/ordermanagement/order_management_api/security/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.leandroftm.ordermanagement.order_management_api.security.config;
 
 import com.leandroftm.ordermanagement.order_management_api.security.filter.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
