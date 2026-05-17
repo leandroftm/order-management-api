9b843b9b5600fdf8423ddd97b2a37315027c5aec
Leandro Torres <leandroftm@gmail.com>
2026-05-17 02:40:38 -0300

    refactor: removed unused imports

--- src/test/java/com/leandroftm/ordermanagement/order_management_api/controller/AuthControllerTest.java
+++ src/test/java/com/leandroftm/ordermanagement/order_management_api/controller/AuthControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
