9c35b55909c13e2c44ae00e6432f39db994d2f45
Leandro Torres <leandroftm@gmail.com>
2026-05-19 00:33:03 -0300

    feat: created tests for order, with: GET(admin) order, GET(admin) orders, get(admin) orders by status, POST create order, POST pay order, POST cancel order

---
+++ src/test/java/com/leandroftm/ordermanagement/order_management_api/controller/OrderControllerTest.java
@@ -0,0 +1,153 @@
+package com.leandroftm.ordermanagement.order_management_api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.create.CreateOrderItemRequest;
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.request.create.CreateOrderRequest;
+import com.leandroftm.ordermanagement.order_management_api.domain.dto.response.OrderResponse;
+import com.leandroftm.ordermanagement.order_management_api.domain.entity.User;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Role;
+import com.leandroftm.ordermanagement.order_management_api.domain.enums.Status;
+import com.leandroftm.ordermanagement.order_management_api.security.entity.UserPrincipal;
+import com.leandroftm.ordermanagement.order_management_api.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    OrderService orderService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void shouldReturnOrderSuccessfully() throws Exception {
+        OrderResponse response = new OrderResponse(
+                1L,
+                new BigDecimal("500.00"),
+                LocalDateTime.now().minusDays(1L),
+                Status.CREATED,
+                1L,
+                List.of()
+        );
+
+        when(orderService.getOrder(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/orders/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(response)))
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$.id").value(1)));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void shouldReturnAllOrdersSuccessfully() throws Exception {
+        Page<OrderResponse> page = createPage();
+
+        when(orderService.getAllOrders(any(), any(Pageable.class))).thenReturn(page);
+
+        mockMvc.perform(get("/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(page)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect((jsonPath("$.content[*].id").value(1)));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void shouldReturnAllOrdersByStatusSuccessfully() throws Exception {
+        Page<OrderResponse> page = createPage();
+
+        when(orderService.getAllOrders(any(Status.class), any(Pageable.class))).thenReturn(page);
+
+        mockMvc.perform(get("/orders")
+                        .param("status", "CREATED")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(page)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect((jsonPath("$.content[*].id").value(1)));
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void shouldCreateOrderSuccessfully() throws Exception {
+        CreateOrderRequest request = new CreateOrderRequest(
+                List.of(
+                        new CreateOrderItemRequest(
+                                10,
+                                1L
+                        )
+                )
+        );
+
+        User user = new User("test@test.com", "123456", Role.USER);
+        ReflectionTestUtils.setField(user, "id", 1L);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        when(orderService.createOrder(1L, request)).thenReturn(1L);
+
+        mockMvc.perform(post("/orders")
+                        .with(user(userPrincipal))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void shouldPayOrderSuccessfully() throws Exception {
+      mockMvc.perform(post("/orders/{id}/pay", 1L))
+              .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void shouldCancelOrderSuccessfully() throws Exception {
+        mockMvc.perform(post("/orders/{id}/cancel", 1L))
+                .andExpect(status().isNoContent());
+    }
+
+    private Page<OrderResponse> createPage() {
+        return new PageImpl<>(
+                List.of(new OrderResponse(
+                        1L,
+                        new BigDecimal("500.00"),
+                        LocalDateTime.now().minusDays(1L),
+                        Status.CREATED,
+                        1L,
+                        List.of()
+                )));
+    }
+
+}
